99a99e33f1e40ef9a6cd37a0bbf3f7d64b40aa3e
J Roberto Jimenez <tijuanapaint@gmail.com>
2026-03-03 15:05:13 -0800

    Create graph.py

---
+++ netp/graph.py
@@ -0,0 +1,47 @@
+from collections import defaultdict, deque
+
+
+class ExecutionNode:
+    def __init__(self, node_id, tool_name, args):
+        self.node_id = node_id
+        self.tool_name = tool_name
+        self.args = args
+        self.dependencies = []
+        self.result = None
+
+
+class ExecutionGraph:
+    def __init__(self):
+        self.nodes = {}
+        self.adjacency = defaultdict(list)
+        self.in_degree = defaultdict(int)
+
+    def add_node(self, node: ExecutionNode):
+        self.nodes[node.node_id] = node
+
+    def add_edge(self, from_node, to_node):
+        self.adjacency[from_node].append(to_node)
+        self.in_degree[to_node] += 1
+        self.nodes[to_node].dependencies.append(from_node)
+
+    def topological_sort(self):
+        queue = deque()
+        for node_id in self.nodes:
+            if self.in_degree[node_id] == 0:
+                queue.append(node_id)
+
+        sorted_nodes = []
+
+        while queue:
+            current = queue.popleft()
+            sorted_nodes.append(current)
+
+            for neighbor in self.adjacency[current]:
+                self.in_degree[neighbor] -= 1
+                if self.in_degree[neighbor] == 0:
+                    queue.append(neighbor)
+
+        if len(sorted_nodes) != len(self.nodes):
+            raise RuntimeError("Cycle detected in execution graph")
+
+        return sorted_nodes
